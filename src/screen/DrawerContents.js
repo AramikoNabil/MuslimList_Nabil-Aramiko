bca98675dd57978cc9f81855083cbdbb90b33766
sleepy81k <nabilnico@gmail.com>
2020-10-12 08:45:49 +0700

    "UpdateScroolView"

--- src/screen/DrawerContents.js
+++ src/screen/DrawerContents.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {View, Text, Image, TouchableOpacity, ScrollView} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import Homepage from './Homepage';
 import Avatar from '../assets/icon/ava.png';
@@ -19,40 +19,42 @@ const RootDrawer = () => {
 
 const DrawerContent = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Image source={Avatar} style={styles.Avatar} />
-      </View>
-      <View style={styles.User}>
-        <Text style={styles.Name}>Aramiko Nabil</Text>
-        <Text style={styles.username}>@username</Text>
-        <Text style={styles.font1}>
-          202 <Text style={styles.font2}>Followers </Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image source={Avatar} style={styles.Avatar} />
+        </View>
+        <View style={styles.User}>
+          <Text style={styles.Name}>Aramiko Nabil</Text>
+          <Text style={styles.username}>@username</Text>
           <Text style={styles.font1}>
-            202 <Text style={styles.font2}>Followers</Text>
+            202 <Text style={styles.font2}>Followers </Text>
+            <Text style={styles.font1}>
+              202 <Text style={styles.font2}>Followers</Text>
+            </Text>
           </Text>
-        </Text>
-      </View>
-      <View style={styles.BodyIcon}>
-        <Image source={starIcon} style={styles.Icon} />
-        <Text style={styles.fontPopular}>Popular</Text>
-      </View>
-      <View style={styles.BodyIcon}>
-        <Image source={locateIcon} style={styles.LocateIcon} />
-        <Text style={styles.fontPopular}>Bookmarks</Text>
-      </View>
-      <View>
-        <TouchableOpacity
-          style={styles.buttonSignOut}
-          onPress={() => this.props.navigation.navigate('GetStarted')}>
-          <Text
-            style={styles.fontSignOut}
+        </View>
+        <View style={styles.BodyIcon}>
+          <Image source={starIcon} style={styles.Icon} />
+          <Text style={styles.fontPopular}>Popular</Text>
+        </View>
+        <View style={styles.BodyIcon}>
+          <Image source={locateIcon} style={styles.LocateIcon} />
+          <Text style={styles.fontPopular}>Bookmarks</Text>
+        </View>
+        <View>
+          <TouchableOpacity
+            style={styles.buttonSignOut}
             onPress={() => this.props.navigation.navigate('GetStarted')}>
-            Sign Out
-          </Text>
-        </TouchableOpacity>
+            <Text
+              style={styles.fontSignOut}
+              onPress={() => this.props.navigation.navigate('GetStarted')}>
+              Sign Out
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
--- src/screen/Registration.js
+++ src/screen/Registration.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   Picker,
   KeyboardAvoidingView,
+  ScrollView,
 } from 'react-native';
 import {styles} from '../styles/Registration/Registration';
 import iconBack from '../assets/icon/arrow-left.png';
@@ -15,108 +16,113 @@ import eyeIcon from '../assets/icon/Shape.png';
 class Registration extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-          <TouchableOpacity
-            style={styles.headBarIcon}
-            onPress={() => this.props.navigation.goBack()}>
-            <Image
-              source={iconBack}
-              style={styles.barIcon}
-              onPress={() => this.props.navigation.goBack()}
-            />
-          </TouchableOpacity>
-        </View>
-        <View>
-          <Text style={styles.Header}>Registration</Text>
-        </View>
-        <View>
-          <Text style={styles.fontBody}>
-            Already have an account?{' '}
-            <Text
-              style={styles.fontLogin}
-              onPress={() => this.props.navigation.navigate('Login')}>
-              Login
-            </Text>
-          </Text>
-        </View>
-        <View>
-          <Text style={styles.fontEmail}>Email</Text>
-        </View>
-        <KeyboardAvoidingView>
+      <ScrollView>
+        <View style={styles.container}>
           <View>
-            <TextInput
-              style={styles.fontInputPw}
-              placeholder="example@gmail.com"
-              keyboardType="email-address"
-            />
-          </View>
-          <View style={styles.mobileNumber}>
-            <Text>Mobile Number</Text>
-          </View>
-          <View style={styles.number}>
-            <Picker style={styles.picker}>
-              <Picker.Item label="+62" value="+62" />
-              <Picker.Item label="+4343" value="+4343" />
-            </Picker>
-            <TextInput style={styles.inputNumber} keyboardType="number-pad" />
-          </View>
-          <View>
-            <Text style={styles.fontPassword}>Password</Text>
-          </View>
-          <View style={styles.eyePassword}>
-            <TextInput
-              style={styles.fontInputPass}
-              underlineColorAndroid="transparent"
-              placeholder="Password"
-              secureTextEntry={true}
-              // secureTextEntry={(show) => this.show(show)}
-            />
             <TouchableOpacity
-              // onPress={(show) => this.setShow(!show)}
-              style={styles.eye}>
-              <Image source={eyeIcon} />
+              style={styles.headBarIcon}
+              onPress={() => this.props.navigation.goBack()}>
+              <Image
+                source={iconBack}
+                style={styles.barIcon}
+                onPress={() => this.props.navigation.goBack()}
+              />
             </TouchableOpacity>
           </View>
           <View>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => this.props.navigation.navigate('Login')}>
+            <Text style={styles.Header}>Registration</Text>
+          </View>
+          <View>
+            <Text style={styles.fontBody}>
+              Already have an account?{' '}
               <Text
-                style={styles.buttonText}
+                style={styles.fontLogin}
                 onPress={() => this.props.navigation.navigate('Login')}>
-                Sign Up
+                Login
               </Text>
-            </TouchableOpacity>
-          </View>
-
-          <View>
-            <Text style={styles.fontloginAnother}>
-              Log in with social account
             </Text>
           </View>
-          <View style={styles.socialMedia}>
-            <TouchableOpacity onPress={() => {}} style={styles.buttonFacebook}>
-              <Text style={styles.textFacebook}>Facebook</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => {}} style={styles.buttonGoogle}>
-              <Text style={styles.fontGoogle}>Google</Text>
-            </TouchableOpacity>
-          </View>
           <View>
-            <Text style={styles.fontTermofuse}>
-              By clicking sign up you are agreeing to the{'          '}
-              <Text style={styles.textSignUp}>
-                {'   '}
-                Term of use{' '}
-                <Text style={styles.fontTermofuse}>
-                  and the <Text style={styles.textSignUp}>Privacy policy</Text>
+            <Text style={styles.fontEmail}>Email</Text>
+          </View>
+          <KeyboardAvoidingView>
+            <View>
+              <TextInput
+                style={styles.fontInputPw}
+                placeholder="example@gmail.com"
+                keyboardType="email-address"
+              />
+            </View>
+            <View style={styles.mobileNumber}>
+              <Text>Mobile Number</Text>
+            </View>
+            <View style={styles.number}>
+              <Picker style={styles.picker}>
+                <Picker.Item label="+62" value="+62" />
+                <Picker.Item label="+4343" value="+4343" />
+              </Picker>
+              <TextInput style={styles.inputNumber} keyboardType="number-pad" />
+            </View>
+            <View>
+              <Text style={styles.fontPassword}>Password</Text>
+            </View>
+            <View style={styles.eyePassword}>
+              <TextInput
+                style={styles.fontInputPass}
+                underlineColorAndroid="transparent"
+                placeholder="Password"
+                secureTextEntry={true}
+                // secureTextEntry={(show) => this.show(show)}
+              />
+              <TouchableOpacity
+                // onPress={(show) => this.setShow(!show)}
+                style={styles.eye}>
+                <Image source={eyeIcon} />
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => this.props.navigation.navigate('Login')}>
+                <Text
+                  style={styles.buttonText}
+                  onPress={() => this.props.navigation.navigate('Login')}>
+                  Sign Up
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <View>
+              <Text style={styles.fontloginAnother}>
+                Log in with social account
+              </Text>
+            </View>
+            <View style={styles.socialMedia}>
+              <TouchableOpacity
+                onPress={() => {}}
+                style={styles.buttonFacebook}>
+                <Text style={styles.textFacebook}>Facebook</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => {}} style={styles.buttonGoogle}>
+                <Text style={styles.fontGoogle}>Google</Text>
+              </TouchableOpacity>
+            </View>
+            <View>
+              <Text style={styles.fontTermofuse}>
+                By clicking sign up you are agreeing to the{'          '}
+                <Text style={styles.textSignUp}>
+                  {'               '}
+                  Term of use{' '}
+                  <Text style={styles.fontTermofuse}>
+                    and the{' '}
+                    <Text style={styles.textSignUp}>Privacy policy</Text>
+                  </Text>
                 </Text>
               </Text>
-            </Text>
-          </View>
-        </KeyboardAvoidingView>
-      </View>
+            </View>
+          </KeyboardAvoidingView>
+        </View>
+      </ScrollView>
     );
   }
 }
